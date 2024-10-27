3f05b0158e46528bede183d77ef00000c9ad5b48
João Marcos <jmarcosmmn@gmail.com>
2024-10-26 23:04:20 -0300

    feat: adding department and automatic creation of dishes

--- marinheirosorders/core/serializers.py
+++ marinheirosorders/core/serializers.py
@@ -7,22 +7,36 @@ class DishSerializer(serializers.ModelSerializer):
         fields = ['id', 'department', 'dish_name']
 
 class OrderDishSerializer(serializers.ModelSerializer):
+    dish = DishSerializer()
+
     class Meta:
         model = OrderDish
         fields = ['dish', 'amount', 'dish_note']  # Campos do OrderDish
 
 class OrderSerializer(serializers.ModelSerializer):
-    order_dishes = OrderDishSerializer(many=True, write_only=True)  # Usando o novo serializer
+    order_dishes = OrderDishSerializer(many=True)  # Usando o novo serializer
 
     class Meta:
         model = Order
         fields = ['id', 'date_time', 'table_number', 'waiter', 'is_outside', 'order_note', 'order_dishes']
 
     def create(self, validated_data):
+        # Extraímos dados de `order_dishes`
         order_dishes_data = validated_data.pop('order_dishes')
+        # Criamos o pedido
         order = Order.objects.create(**validated_data)
+
         for order_dish_data in order_dishes_data:
-            OrderDish.objects.create(order=order, **order_dish_data)
+            # Extraímos o dado do prato
+            dish_data = order_dish_data.pop('dish')
+            # Buscamos ou criamos a instância do Dish
+            dish, created = Dish.objects.get_or_create(
+                department=dish_data['department'],
+                dish_name=dish_data['dish_name']
+            )
+            # Criamos o OrderDish associado ao pedido e ao prato
+            OrderDish.objects.create(order=order, dish=dish, **order_dish_data)
+
         return order
 
     def to_representation(self, instance):
